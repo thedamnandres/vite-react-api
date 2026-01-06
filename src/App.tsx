605117e516914fbc48c2ea9056c2d7616cd810a5
Andres Jimenez <andres_jimenez666@hotmail.com>
2026-01-05 19:46:17 -0500

    3

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <img src="https://media4.giphy.com/media/v1.Y2lkPTZjMDliOTUya2t5eHJ3aDA5OGpiMDhscHk0a3Zub3hlcm5vNXFjcHpnbGNpczVnMCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Nx0rz3jtxtEre/source.gif" alt="GIF" />
   )
